c7af0efbc925df58b6baf7b1dde5f45868789e4b
NVK222 <neerajvk2004@gmail.com>
2026-06-05 12:52:56 +0530

    Changed the search directory & added new exclusions

--- src/tools.py
+++ src/tools.py
@@ -1,27 +1,35 @@
 import subprocess
-
 from langchain.tools import tool
-from pathlib import Path
 from utils import get_project_root
 import os
-import pytest
 
 
 @tool
 def list_files() -> list[str]:
     """
-    List all files in path project_root/src
+    List all files in the project root.
     Returns:
-        All the files in the src directory of project root.
+        All the files in the project root.
     """
-    search_dir = get_project_root() / "src"
+    search_dir = get_project_root()
     files = []
-    ignore = {"__pycache__"}
+
+    # Ignore list
+    ignore = {"__pycache__", ".lock"}
+
+    # TODO : For large directories this may be slow. May need to replace with os cmds
     for file in search_dir.rglob("*"):
+        # Folders / files starting with . are excluded automatically
+        if str(file.relative_to(search_dir)).startswith("."):
+            continue
+
+        # Folders / files in ignore are excluded
         if not file.is_file() or any(
             ignore_file in str(file) for ignore_file in ignore
         ):
             continue
+
+        # Retun path relative to root
         files.append(str(file.relative_to(get_project_root())))
     return files
 
@@ -31,10 +39,11 @@ def open_file(path: str) -> str:
     """
     Opens file at path and returns the contents as string.
     Args:
-        path: A string path relative to the root of the directory, starting with src/
+        path: A string path relative to the project root. It must be one of the paths returned by list_files
     Returns:
         Contents of the file as string if found, else an error message.
     """
+
     file_path = get_project_root() / path
     if not os.path.exists(file_path):
         return f"The file at {path} does not exist."
@@ -47,7 +56,7 @@ def edit_file(path: str, search_block: str, replace_block: str) -> str:
     """
     Safely replaces a specific block of code inside a file.
     Args:
-        path: The relative path to the target file from project root.
+        path: The relative path to the target file from project root. It must be one of the paths returned by list_files
         search_block: The exact, existing code block to find (include indentation).
         replace_block: The new code block to replace the search_block with.
     Returns:
@@ -72,7 +81,7 @@ def run_tests() -> str:
     Runs tests on the whole project and returns the terminal output.
     """
 
-    test_dir = get_project_root() / "src" / "tests"
+    test_dir = get_project_root() / "tests"
     result = subprocess.run(
         ["pytest", "-v", str(test_dir)], capture_output=True, text=True
     )
