a03f952ba905076d6d2fec93e523fbc6ba2d574f
NVK222 <neerajvk2004@gmail.com>
2026-06-17 18:09:27 +0530

    Added CORS

--- src/codedoctor/api/main.py
+++ src/codedoctor/api/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from codedoctor.api.routers.doctor import doctor_router
 from codedoctor.api.routers.engineer import engineer_router
 
 app = FastAPI()
 
+origins = ["http://localhost:5173"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["GET", "POST", "OPTIONS"],
+    allow_headers=["*"],
+)
 app.include_router(doctor_router)
 app.include_router(engineer_router)
